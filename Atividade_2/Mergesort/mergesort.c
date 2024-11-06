970d10b00d94111172c07a6a80842f87c0024b5a
Daniel H. <daniel.neves0021@gmail.com>
2024-11-06 08:31:32 -0300

    feat: mergersort added

--- Atividade_2/Mergesort/mergesort.c
+++ Atividade_2/Mergesort/mergesort.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
-#define ARRAY_LENGHT 5
+#define ARRAY_LENGHT 9
 void printArray( int* v, int l ); 
+void mergesort( int* v, int l ); 
 int main( int arg, int args[] )
 {
-				int v[ARRAY_LENGHT] = {5,3,4,2,1};
+				int v[ARRAY_LENGHT] = {5,3,4,2,1,7,6,8,9};
+				printArray( v, ARRAY_LENGHT );
+				mergesort(v,ARRAY_LENGHT);
 				printArray( v, ARRAY_LENGHT );
 				return 1;
 }
@@ -15,12 +18,17 @@ void swap( int* v, int i, int j )
 }
 void mergesort( int* v, int l ) 
 {
-
+				int ray, temp = 0;
 				for ( int i = 0 ; i < l ; i ++ ) 
-								for ( int j = 0 ; j < l ; j ++ ) 
+				{
+								ray = (int)((l - temp) / 2);
+								temp = ray;
+								for ( int j = 0 ; j < (l - ray) ; j ++ ) 
 								{
-																
+											if ( v[j] > v[i] )
+															swap(v,i,j);
 								}
+				}
 }
 void printArray( int* v, int l ) 
 {
